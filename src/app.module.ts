bc54c99102d223d57e5f1425f518ecbdb3e2784b
Jose Carrillo <jose.carrillo@alumnos.ucn.cl>
2025-10-24 22:04:30 -0300

    Conexión creada con microservicio de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables estén disponibles en toda la app
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
